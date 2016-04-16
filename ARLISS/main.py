6f8d8132b2145ab3088ca13686a384cf798b748a
William <rossgregory36@gmail.com>
2016-04-16 00:37:27 +0000

    update code

--- ARLISS/main.py
+++ ARLISS/main.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 
-# ARLISS QUAD PYTHON CODE
+# ARLISS Quadrotor Mission Code
+
+# History:
+# - WG 03/03/2016: create file and inital code
+# - WG 04/15/2016: remove quad class. rewrite mission.main_run
+
 # Note:
+#	- all distance in meters (m)
+#	- positions [lat,lon]
+#	- Organized as follows:
+#		-Mission
+#			-Move
+#			-Sensors
+#
+# Add:
 #	- way to arm/disarm quad
-#	- default waypoint navigation quality at flying to target in current situation
-#	- recovery effort after ejection
-#	- flight mode heights
-#	- landing command
-
-
-#import sys
-#import math
-import clr
-#import time
-clr.AddReference("MissionPlanner")
-import MissionPlanner
-clr.AddReference("MissionPlanner.Utilities")
-from MissionPlanner.Utilities import Locationwp
-clr.AddReference("MAVLink") # includes the Utilities class
-import MAVLink
+#	- detect launch
+#	- recovery
+#	- waypoint navigation
+#	- landing
 
 # Information about ArduPilot python commands:
 # --------------------------------------------------------------------------------------
@@ -40,6 +41,23 @@ import MAVLink
 # -command messages here: http://plane.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/
 #
 # RC Input and Output values - http://dev.ardupilot.com/wiki/learning-ardupilot-rc-input-output/
+# --------------------------------------------------------------------------------------
+
+#import sys
+#import math
+#import time
+import clr
+clr.AddReference("MissionPlanner")
+import MissionPlanner
+clr.AddReference("MissionPlanner.Utilities")
+from MissionPlanner.Utilities import Locationwp
+clr.AddReference("MAVLink") # includes the Utilities class
+import MAVLink
+
+#
+# Move Class
+# ------------------------------------------------------
+#
 
 class Move:
 	merror = False # true if there is a move related error
@@ -59,9 +77,13 @@ class Move:
 		# http://copter.ardupilot.com/wiki/common-mavlink-mission-command-messages-mav_cmd/#mav_cmd_nav_land
 		# MAV_CMD_NAV_LAND(0,0,0,0,l,l) - NODE: do not know if this command works
 		Script.ChangeMode('LAND') # set mode to LAND - http://copter.ardupilot.com/wiki/land-mode/
-	
+
+#
+# Sensors Class
+# ------------------------------------------------------
+#
+
 class Sensors:
-	serror = False # true if there is a sensor related error
 	current_time = 0
 	current_mode = ''
 	current_distance = 0
@@ -76,9 +98,11 @@ class Sensors:
 	current_yaw = 0.0
 	current_lat = 0.0
 	current_lng = 0.0
+	
 	# init
 	def __init__(self):
 		print("sensors online")
+	
 	# get current flight data
 	def get_flight_data():
 		global current_time, current_mode, current_distance, current_altitude, \
@@ -112,71 +136,89 @@ class Sensors:
 		print("- wind_direction: " + str(current_wind_direction))
 		print("- wind_speed: " + str(current_wind_speed))
 
-class Quad:
+#
+# Mission Class
+# ------------------------------------------------------
+#
+
+class Mission:
 	mov = Move()
 	sen = Sensors()
 	
-	error = False # true if there is any unresolved error
-	
-	def __init__(self):
-		print("quad online")
-	def directed_flight():
-		Move.set_waypoint(target_pos, alt)
-
-class Mission:
-	quad = Quad()
-
-	start_pos = [0.0, 0.0]
-	ejected_pos = [0.0, 0.0]
+	# --- SET BEFORE MISSION ---
 	target_pos = [31.002, -110.010]
+	recovery_start_alt = 1500.0
+	landing_start_alt = 20.0
+	landing_start_dist = 10.0
+	# --- SET BEFORE MISSION ---
 	
-	start_alt = 0.0
-	max_alt = 0.0
-	free_fall_end_alt = 4000.0
-	landing_start_alt = 400.0
-	landing_start_dist = 50.0
-	
-	rocket_launched = False
-	ejected = False
-	
-	flight_mode = 0 # 0:idle, 1:directed_flight, 2:landing
+	# state variables
+	launch_pos = [0.0, 0.0]
+	launch_alt = 0
+	ejected_pos = [0.0, 0.0]
+	rocket_launched = False # true once rocket launched
+	ejected = False # true once released from rocket
+	recovered = False # true once in stable flight
+	landed = False
+	armed = False # set to True to engage system
 	
 	def __init__(self):
 		print("mission online")
+	
 	# call at mission start
 	def start():
 		flight_mode = 0
-		start_alt = quad.sen.current_altitude
-		max_alt = start_alt
-		start_pos = [quad.sen.current_lat, quad.sen.current_lon]
-	# loop for entire mission
+		launch_pos = [sen.current_lat, sen.current_lon]
+		launch_alt = sen.current_altitude
+	
+	# fly to waypoint
+	def directed_flight():
+		mov.set_waypoint(target_pos, alt)
+	
+	# ------------
+	# mission code
+	# ------------
 	def main_run():
 		start()
-		while mission_complete == false:
-			# test until rocket launched
-			while rocket_launched == false:
-				if quad.sen.current_altitude>start_alt+1000: rocket_launched = True
-			print("rocket launched")
-			
-			# test until rocket ejected
-			while ejected == false:
-				if quad.sen.current_altitude<max_alt+1000: ejected = True # NOTE: maybe use accelerometer
-			print("quad ejected")
-			ejected_pos = [quad.sen.curren_lat, quad.sen.curren_lon]
-			
-			# wait until below free fall set alt
-			while quad.sen.current_altitude>free_fall_end_alt: pass
-			flight_mode = 1
-			quad.mov.set_waypoint(target_pos, landing_start_alt+1000)
-			
-			# wait until within landing zone
-			while quad.sen.current_altitude>landing_start_alt and quad.sen.current_distance>landing_start_dist: pass
-			flight_mode = 2
-			quad.move.start_landing()
-			
-		print("mission complete")
+		# test until rocket launched
+		while rocket_launched == false:
+			if sen.current_altitude>start_alt+1000: rocket_launched = True
+		print("rocket launched")
 		
-# run at script start
+		# test until rocket ejected
+		while ejected == false:
+			if sen.current_altitude>max_alt: max_alt = sen.current_altitude
+			if sen.current_altitude<max_alt+200: ejected = True # NOTE: maybe use accelerometer
+		print("quad ejected")
+		ejected_pos = [sen.curren_lat, sen.curren_lon]
+		
+		# wait until recovery start alt
+		while sen.current_altitude>recovery_start_alt:
+			# recover craft!!
+			#if stable: recovered = True
+			pass
+		
+		# navigate to target
+		navigation_phase = True
+		while navigation_phase:
+			# set succsessive waypoints leading to target or just target waypoint??
+			mov.set_waypoint(target_pos, landing_start_alt+1000)
+			if sen.current_altitude < landing_start_alt:
+				#if distance_to_target < landing_start_dist:
+					navigation_phase = False
+
+		mov.start_landing()
+		# test for landed
+		landed = True
+		
+		print("mission complete")
+		armed = False
+	
+#
+# Autostart
+# ------------------------------------------------------
+#
+
 def autostart():
 	print("Hello ARLISS")
 	mission = Mission()
