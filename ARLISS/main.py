3e8ff37e49c1f5193e3ed6655bd91824d1732c1e
William <rossgregory36@gmail.com>
2016-04-29 01:44:56 +0000

    update

--- ARLISS/main.py
+++ ARLISS/main.py
@@ -6,13 +6,7 @@ Version 0.2
 
 repo: https://github.com/RobRight/ARLISS/
 
-Todo:
-	test modes 1-3
-	 - landing
-	 - waypoints
-	 - test sensors
-	test RC control
-		- finish arm/disarm and takeoff
+
 
 '''
 
@@ -77,10 +71,10 @@ class Move:
 	default_takeoff_speed = 2 # m/s
 
 	# RC input [pin, min, max]
-	rc_pin_throttle = [3, Script.GetParam('RC3_MIN'), Script.GetParam('RC3_MAX')]
-	rc_pin_pitch = [2, Script.GetParam('RC4_MIN'), Script.GetParam('RC4_MAX')] # BACKWARDS
-	rc_pin_roll = [1, Script.GetParam('RC5_MIN'), Script.GetParam('RC5_MAX')]
-	rc_pin_yaw = [4, Script.GetParam('RC6_MIN'), Script.GetParam('RC6_MAX')]
+	rc_throttle = [3, Script.GetParam('RC3_MIN'), Script.GetParam('RC3_MAX')]
+	rc_pitch = [2, Script.GetParam('RC4_MIN'), Script.GetParam('RC4_MAX')] # BACKWARDS
+	rc_roll = [1, Script.GetParam('RC5_MIN'), Script.GetParam('RC5_MAX')]
+	rc_yaw = [4, Script.GetParam('RC6_MIN'), Script.GetParam('RC6_MAX')]
 	# --- SETTINGS END ---
 
 	def __init__(self):
@@ -90,40 +84,57 @@ class Move:
 		# Script.SendRC(rc_throttle, 1000, True)
 		pass
 
+	def rc_value_map(chan, val): # val: 0 to 1
+		min_val = chan[1]
+		max_val = chan[2]
+		return (max_val - min_val)*val + min_val
+
+	# call to engage motor at 0 to 1 speed
+	def rc_set_value(chan, in_val):
+		val = rc_value_map(chan, in_val)
+		Script.SendRC(chan[0], val, True)
+
+	# set value to all four rc channels
+	def rc_set_all(in_t, in_p, in_r, in_y):
+		if in_t != -1: rc_set_value(rc_throttle, in_t)
+		if in_p != -1: rc_set_value(rc_pitch, in_p)
+		if in_r != -1: rc_set_value(rc_roll, in_r)
+		if in_y != -1: rc_set_value(rc_yaw, in_y)
+
+	# reset all rc value inputs to idle
+	def rc_reset_all():
+		# throttle min, pitch half, roll half, yaw half
+		rc_set_all(rc_value_map(rc_throttle, 0), rc_value_map(rc_pitch, 0.5), rc_value_map(rc_roll, 0.5), rc_value_map(rc_yaw, 0.5))
+
 	# engage craft for flight
 	# TESTED: False
 	def arm_craft(self):
-		'''
-		print("disarming motors")
+		print("arming motors")
 		change_mode_loiter()
-		Script.SendRC(3, 1000, True)
-		Script.SendRC(4, 1000, True)
-		Script.WaitFor('DISARMING MOTORS',15000)
-		Script.SendRC(4, 1500, True)
-		print("MOTORS DISARMED")
+		rc_reset_all()
+		rc_set_all(0,0,1,1)
+		Script.WaitFor('ARMING MOTORS',15000)
+		rc_reset_all()
+		print("MOTORS ARMED")
 		return True
-		'''
-		pass
 
 	# disarm motors (low power mode)
 	# TESTED: False
 	def disarm_craft(self):
-		'''
 		print("disarming motors")
 		change_mode_loiter()
-		Script.SendRC(3, 1000, True)
-		Script.SendRC(4, 1000, True)
+		rc_reset_all()
+		rc_set_all(0,0,0,0)
 		Script.WaitFor('DISARMING MOTORS',15000)
-		Script.SendRC(4, 1500, True)
+		rc_reset_all()
 		print("MOTORS DISARMED")
 		return True
-		'''
-		pass
 
+	# test of rc control
 	def test_rc(self):
-		pin = rc_pin_throttle[0]
-		min_val = rc_pin_throttle[1]
-		max_val = rc_pin_throttle[2]
+		pin = rc_throttle[0]
+		min_val = rc_throttle[1]
+		max_val = rc_throttle[2]
 		Script.SendRC(pin, max_val/2, True)
 		time.sleep(2)
 		Script.SendRC(pin, min_val, True)
@@ -163,7 +174,6 @@ class Move:
 	# takeoff for testing and non-assisted flying
 	# TESTED: False
 	def takeoff_from_ground(self):
-		'''
 		takeoff_alt = default_takeoff_alt
 		takeoff_speed = default_takeoff_speed
 		# must do manually?
@@ -174,7 +184,7 @@ class Move:
 		# enter loiter mode
 		mov.change_mode_loiter()
 		# engage motors
-		Script.SendRC(rc_pin_throttle, 1000, True)
+		Script.SendRC(rc_throttle, 1000, True)
 		# monitor vertical speed
 		if current_verticle_speed < takeoff_speed-2:
 			throttle+1
@@ -183,7 +193,10 @@ class Move:
 		# maintain until at set altitude
 		if current_altitude > sen.temp_start_alt + takeoff_alt:
 			pass
-		'''
+
+	# setup motor code stuff
+	def setup(self):
+		rc_reset_all()
 
 #
 # Sensors Class
@@ -271,7 +284,24 @@ class Sensors:
 		print("- ground_course: " + str(current_ground_course))
 		print("- wind_direction: " + str(current_wind_direction))
 		print("- wind_speed: " + str(current_wind_speed))
+		print("- rol: " + str(current_roll))
+		print("- pitch: " + str(current_pitch))
+		print("- yaw: " + str(current_yaw))
+		print("- latitude: " + str(current_lat))
+		print("- longitude: " + str(current_lng))
+		print("- gps_stat: " + str(current_gps_stat))
+		print("- gps_count: " + str(current_gps_count))
+		print("- battery_voltage: " + str(current_battery_voltage))
+		print("- armed: " + str(current_armed))
+		print("- altitude_error: " + str(current_altitude_error))
+		print("- accel.x: " + str(current_accel[0]))
+		print("- accel.y: " + str(current_accel[1])))
+		print("- accel.z: " + str(current_accel[2]))
+		print("- gyro.x: " + str(current_gyro[0]))
+		print("- gyro.y: " + str(current_gyro[1]))
+		print("- gyro.z: " + str(current_gyro[2]))
 
+	# make one string with logged data
 	def return_string_of_data():
 		sen_data = "sensors current:" + "\n" + \
 		"- time: " + str(current_time) + "\n" + \
@@ -283,9 +313,26 @@ class Sensors:
 		"- ground_speed: " + str(current_ground_speed) + "\n" + \
 		"- ground_course: " + str(current_ground_course) + "\n" + \
 		"- wind_direction: " + str(current_wind_direction) + "\n" + \
-		"- wind_speed: " + str(current_wind_speed) + "\n" + "\n"
+		"- wind_speed: " + str(current_wind_speed) + "\n" + \
+		"- rol: " + str(current_roll) + "\n" + \
+		"- pitch: " + str(current_pitch) + "\n" + \
+		"- yaw: " + str(current_yaw) + "\n" + \
+		"- latitude: " + str(current_lat) + "\n" + \
+		"- longitude: " + str(current_lng) + "\n" + \
+		"- gps_stat: " + str(current_gps_stat) + "\n" + \
+		"- gps_count: " + str(current_gps_count) + "\n" + \
+		"- battery_voltage: " + str(current_battery_voltage) + "\n" + \
+		"- armed: " + str(current_armed) + "\n" + \
+		"- altitude_error: " + str(current_altitude_error) + "\n" + \
+		"- accel.x: " + str(current_accel[0]) + "\n" + \
+		"- accel.y: " + str(current_accel[1]) + "\n" + \
+		"- accel.z: " + str(current_accel[2]) + "\n" + \
+		"- gyro.x: " + str(current_gyro[0]) + "\n" + \
+		"- gyro.y: " + str(current_gyro[1]) + "\n" + \
+		"- gyro.z: " + str(current_gyro[2]) + "\n" + "\n"
 		return sen_data
 
+	# log flight data to file
 	def log_data(self):
 		sen_data = self.return_string_of_data()
 		# log data
@@ -315,6 +362,7 @@ class Mission:
 	mission_mode = 0
 
 	requrie_gps_lock = True
+	testing = True
 
 	verbose = True
 	debug = True
@@ -356,6 +404,7 @@ class Mission:
 		self.log.log_enable = self.log_data
 		start_time = time.localtime()
 		self.log.start_time = start_time
+		mov.setup()
 
 	# Utils ------------------
 	def print_intro(self):
@@ -440,6 +489,32 @@ class Mission:
 	def check_within_landing(self):
 		pass
 
+	def disarm_craft():
+		self.armed = False
+
+	def land_craft():
+		self.mov.start_landing()
+		self.landed = True
+
+	def set_mission_text():
+		# (0) no mission
+		if self.mission_mode == 0:
+			self.mission_text = 'no mission'
+		# (1) test_landing
+		elif self.mission_mode == 1:
+			self.mission_text = 'test_landing'
+		# (2) test_navigation
+		elif self.mission_mode == 2:
+			self.mission_text = 'test_navigation'
+		# (3) test_sensors
+		elif self.mission_mode == 3:
+			self.mission_text = 'test_sensors'
+		# (11) - mission_alpha -
+		elif self.mission_mode == 11:
+			self.mission_text = 'mission_alpha'
+		else:
+			print("error: mission_mode unknown")
+
 	# Test -------------------
 	# take off from ground, reach set altitude and loiter
 	def test_takeoff(self):
@@ -476,6 +551,7 @@ class Mission:
 			self.sen.log_data()
 			# loop twice a second
 			time.sleep(1)
+			if self.debug: print("- done")
 
 	# Main ------------------
 	# recover craft from unstable flight
@@ -484,78 +560,66 @@ class Mission:
 
 	# fly to waypoint
 	def phase_directed_flight(self):
-		mov.set_waypoint(save_loc_0)
+		if self.debug: print("starting: phase_directed_flight")
+		self.navigation_phase = True
+		while self.navigation_phase:
+			# set successive waypoints leading to target or just target waypoint??
+			self.mov.set_waypoint(self.target_pos, self.landing_start_alt+1000)
+			if self.check_within_landing() == True:
+				self.navigation_phase = False
+		if self.debug: print("- done")
 
-	# -------------------------
 	# ------------
 	# mission alpha code start
 	# ------------
 	def mission_alpha(self):
-		start()
-
-		wait_for_rocket_launch()
-		wait_for_rocket_ejected()
-
+		# wait for launch
+		self.wait_for_rocket_launch()
+		self.wait_for_rocket_ejected()
 		# wait until recovery start alt
-		wait_for_altitude(recovery_start_alt, True)
-
+		self.wait_for_altitude(recovery_start_alt, True)
 		# begin recovery
-		recover_craft()
-
+		self.recover_craft()
 		# navigate to target
-		navigation_phase = True
-		while navigation_phase:
-			# set successive waypoints leading to target or just target waypoint??
-			mov.set_waypoint(target_pos, landing_start_alt+1000)
-			# reached the target?
-			if check_within_landing() == True:
-				navigation_phase = False
-
-		mov.start_landing()
-		landed = True
-
-		print("mission complete")
-		armed = False
+		self.phase_directed_flight()
+		# land then disarm
+		self.land_craft()
+		self.disarm_craft()
 	# ------------
 	# mission alpha code end
 	# ------------
 
 	# main mission code runner
 	def run_main(self):
+		self.start()
 		while self.mission_complete == False:
 			if self.debug: print("starting: run_main")
-			# MAIN RUN CODE HERE
-			# depends on current mission
 			# (0) no mission
 			if self.mission_mode == 0:
-				self.mission_text = 'no mission'
+				pass
 			# (1) test_landing
 			elif self.mission_mode == 1:
-				self.mission_text = 'test_landing'
 				self.test_landing()
 			# (2) test_navigation
 			elif self.mission_mode == 2:
-				self.mission_text = 'test_navigation'
 				self.test_waypoint(False)
 			# (3) test_sensors
 			elif self.mission_mode == 3:
-				self.mission_text = 'test_sensors'
 				self.loop_sensor_output()
 			# (11) - mission_alpha -
 			elif self.mission_mode == 11:
-				self.mission_text = 'mission_alpha'
-				#self.mission_alpha()
+				self.mission_alpha()
 			else:
 				print("error: mission_mode unknown")
-			# mission complete
 			self.mission_complete = True
+			if self.debug: print("- done")
 
 	# call at mission start
 	def run_startup(self):
 		while self.mission_complete == False:
 			if self.debug: print("starting: run_startup")
 			self.print_intro()
-			#time.sleep(5)
+			time.sleep(5)
 			self.warm_up()
 			self.setup()
 			self.print_start()
